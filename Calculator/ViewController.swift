cc9a6ac9bf561d0d339e1d91eb63afe5c209e2ec
Melissa Elliston-Boyes <melissa.elliston-boyes@trademe.co.nz>
2024-12-12 16:07:06 +1300

    Learned about access levels and added private var

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -12,14 +12,12 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var displayLabel: UILabel!
     
-    var isFinishedTypingNumber: Bool = true
+    private var isFinishedTypingNumber: Bool = true
     
     @IBAction func calcButtonPressed(_ sender: UIButton) {
         //What should happen when a non-number button is pressed
         isFinishedTypingNumber = true
-        
     }
-
     
     @IBAction func numButtonPressed(_ sender: UIButton) {
         //What should happen when a number is entered into the keypad
